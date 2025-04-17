4c8c79ab05469568b5b2a3f3105326665a1b6275
keshav2025 <keshavgupta86036@gmail.com>
2025-04-18 02:24:36 +0530

    Updated code

--- src/components/firebase.jsx
+++ src/components/firebase.jsx
@@ -6,7 +6,7 @@ import {getFirestore} from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
+// const firebaseConfig = { remove all the api key
 //   , // dummy data
 //   // removed
 // };
